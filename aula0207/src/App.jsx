54f9845d3eee0256ec4e2eac6ec42a9532cb59c0
Karyne Ohara Freitas de Medeiros <20221101110005@ifrn.local>
2025-07-02 14:28:29 -0300

    Código de Iuri - Atividade sobre listas

---
+++ aula0207/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ListaDeTarefas from './componentes/ListadeTarefas'
+import Listar from './componentes/Listar'
+
+function App() {
+  
+
+  return (
+    <>
+    {/* <ListaDeTarefas/> */}
+    <Listar/>
+    </>
+  )
+}
+
+export default App
